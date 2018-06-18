3e4a5017235956c03612ed9f0fcf9d8bd38d6f67
Sabrina Kennedy <skenne21@gmail.com>
2018-06-18 08:40:04 -0600

    setup of problem:

---
+++ bracketMatcher/script.js
@@ -0,0 +1,20 @@
+
+const bracket = (string) => {
+  console.log(string)
+}
+
+
+
+
+console.log(bracket('{}'));
+// true
+// console.log(bracket('{('));
+// // false
+// console.log(bracket('{()}'));
+// // true
+// console.log(bracket('{[)][]}'));
+// // fasle
+// console.log(bracket(']'));
+// // fasle
+// console.log(bracket('({[]}{[]})'));
+// // true
